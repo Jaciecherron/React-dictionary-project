6e6bf911a638eedfc47316508903090031d9ac2b
Jaciecherron <jaciecherron@gmail.com>
2025-03-10 14:32:41 -0600

    Removed excess files and added footer

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <footer>
+      This project was codded by{" "}
+      <a
+        href="https://github.com/Jaciecherron"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Jacie Spivey{" "}
+      </a>{" "}
+      and is hosted on{" "}
+      <a
+        href="https://dainty-granita-d17123.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+    </footer>
+  );
+}
