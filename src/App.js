bc43ec2b250282033b51186e4cdb5f0d042d4e05
Jaciecherron <jaciecherron@gmail.com>
2025-03-15 11:04:40 -0600

    Added search form and look up capabilities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import './App.css';
+import booklogo from "./booklogo.png"
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
-    <footer>
-      This project was codded by{" "}
-      <a
-        href="https://github.com/Jaciecherron"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Jacie Spivey{" "}
-      </a>{" "}
-      and is hosted on{" "}
-      <a
-        href="https://dainty-granita-d17123.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Netlify
-      </a>
-    </footer>
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={booklogo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            This project was codded by{" "}
+            <a
+              href="https://github.com/Jaciecherron"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Jacie Spivey{" "}
+            </a>{" "}
+            and is hosted on{" "}
+            <a
+              href="https://dainty-granita-d17123.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
+    </div>
   );
 }
