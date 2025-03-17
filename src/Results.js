aafee297893ef6c77243c2f2a806256a2bffaea5
Jaciecherron <jaciecherron@gmail.com>
2025-03-17 09:03:27 -0600

    fixed maping attempt

--- src/Results.js
+++ src/Results.js
@@ -3,14 +3,11 @@ import React from "react";
 export default function Result(props) {
   console.log(props.results);
   if (props.results) {
+    return (
 <div className="results">
-<h2>
-  {props.results.word}
-</h2>
-{props.results.meanings.map(function(meaning, index) {
-  return meaning.definitions[0].definition
-})}
-</div>
+  <h2>{props.results.word}</h2>
+  <p>{props.results.definition}</p>
+</div>);
 } else {
   return null;
 }}
