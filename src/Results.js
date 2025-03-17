018e9fc8e713e971e8bc1eda2552f9947199d6f4
Jaciecherron <jaciecherron@gmail.com>
2025-03-17 13:28:25 -0600

    Expanded results and minor styling

--- src/Results.js
+++ src/Results.js
@@ -4,10 +4,15 @@ export default function Result(props) {
   console.log(props.results);
   if (props.results) {
     return (
-<div className="results">
-  <h2>{props.results.word}</h2>
-  <p>{props.results.definition}</p>
-</div>);
+      <div className="results">
+        <h2>{props.results.word}</h2>
+        <h3>{props.results.partOfSpeech}</h3>
+        <p>{props.results.definition}</p>
+        <p>
+          <strong>Example:</strong> <em>{props.results.example}</em>
+        </p>
+      </div>
+    );
 } else {
   return null;
 }}
