638c0e36d38a1c1e4fb4e96b782bc709dcfd6d2c
Jaciecherron <jaciecherron@gmail.com>
2025-03-30 13:31:14 -0600

    Added Synonym and Example component

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example"
 
 export default function Result(props) {
   console.log(props.results);
@@ -7,9 +9,15 @@ export default function Result(props) {
       <div className="results">
         <h2>{props.results.word}</h2>
         <h3>{props.results.partOfSpeech}</h3>
-        <p>{props.results.definition}</p>
         <p>
-          <strong>Example:</strong> <em>{props.results.example}</em>
+          <strong>Definition: </strong>
+          {props.results.definition}
+        </p>
+        <p>
+          <Example example={props.results.example} />
+        </p>
+        <p>
+          <Synonyms synonyms={props.results.synonyms} />
         </p>
       </div>
     );
---
+++ src/Synonyms.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms){
+    return (
+    <ul className="synonyms">
+    {props.synonyms.map(function(synonyms, index) {
+    return <li key={index}>{synonyms} </li>;
+     })}
+     </ul>
+    );
+  } else {
+    return null;
+  }}
